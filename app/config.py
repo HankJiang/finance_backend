32c02a2251eb019ab2a21122015b5acc61ff5b62
HankJiang <jianghan.ah@foxmail.com>
2020-09-22 11:56:50 +0800

    docker

--- app/config.py
+++ app/config.py
@@ -47,8 +47,8 @@ class Production(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # celery-redis
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = 'redis://redis/0'
+    CELERY_RESULT_BACKEND = 'redis://redis/0'
     CELERYBEAT_SCHEDULE = schedules
     CELERY_IMPORTS = ["app.tasks"]
 
